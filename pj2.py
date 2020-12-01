d05ba26f12413d9e7017bd33ddfc28a8bbfad7b2
BenFrancis <bneto@computacao.cua.ufmt.br>
2020-12-01 16:11:39 -0300

    obtendo centroides

--- pj2.py
+++ pj2.py
@@ -11,23 +11,29 @@ import numpy as np
 from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
-
-
-dados = pd.read_csv('filtered_dataset', sep= ';')
 '''
 1-cura
 2-obito
 3-obito outras causas
 9-ignorado
 ''' 
+
+dados = pd.read_csv('filtered_dataset', sep= ';')
+
+''' .fillna(0) troca valores Nan por 0'''
 clss = dados[['EVOLUCAO','NU_IDADE_N']].fillna(0)
 '''removendo tudo que nao seja cura e obito'''
 clss = clss.drop(clss[clss['EVOLUCAO'] > 2 ].index)
 '''removendo valores nao informados'''
 clss = clss.drop(clss[clss['EVOLUCAO'] < 1 ].index)
-''' .fillna(0) troca valores Nan por 0'''
-kmeans = KMeans(n_clusters=2,verbose=2)
-kmeans.fit(clss)
-y_kmeans = kmeans.predict(clss)
+
+'''organizando kmeans'''
+cluster = KMeans(n_clusters = 2)
+cluster.fit(clss)
+y_kmeans = cluster.predict(clss)
+
+#visualizando centroides
+centroides = cluster.cluster_centers_
+centroides
 
 
