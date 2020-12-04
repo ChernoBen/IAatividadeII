b0b1ab0f654f9a2eb192d476143099c48b1de0ea
benjamim <bneto@computacao.cua.ufmt.br>
2020-12-04 11:14:05 -0300

    kaio e filipe

--- pj2.py
+++ pj2.py
@@ -50,58 +50,14 @@ resultados = confusion_matrix(classe[0],previsoes)
 
 #geração do grafico com os clusters gerados, considerando para um (previsoes 0,1 ou 2)
 #Usamos somente as colunas 0 e 1 da base de dados original para termos 2 dimensoes
-'''plt.scatter(clss[previsoes == 0, 0],clss[previsoes == 0, 1],
+plt.scatter(clss[previsoes == 0, 0],clss[previsoes == 0, 1],
             c = 'green',label = 'obtios')
 plt.scatter(clss[previsoes == 1, 0],clss[previsoes == 1, 1],
             c = 'red',label = 'cura')
 
-plt.legend()''' 
+plt.legend() 
 #tentativa plot
 
-
-from sklearn.metrics import pairwise_distances_argmin
-
-def find_clusters(X, n_clusters, rseed=2):
-    # 1. Randomly choose clusters
-    rng = np.random.RandomState(rseed)
-    i = rng.permutation(X.shape[0])[:n_clusters]
-    centers = X[i]
-    
-    centers_his = []
-    labels_his = []
-    
-    while(True):
-        # 2a. Assign labels based on closest center
-        labels = pairwise_distances_argmin(X, centers)
-        
-        # 2b. Find new centers from means of points
-        new_centers = np.array([X[labels == i].mean(0)
-                                for i in range(n_clusters)])
-        
-        # 2c. Check for convergence
-        if np.all(centers == new_centers):
-            break
-        centers = new_centers
-        
-        centers_his.append(centers)
-        labels_his.append(labels)
-    
-    return centers, labels, centers_his, labels_his
-
-
-   
-centers, labels, centers_his, labels_his = find_clusters(classe, 2)
-#plot centroides
-for i in range(len(centers_his)):
-    centers = centers_his[i]
-    labels = labels_his[i]
-    fig = plt.figure(figsize=(7, 5))
-    fig.set_tight_layout(True)
-    plt.scatter(classe[:, 0], classe[:, 1], c=labels,
-                s=50, cmap='rainbow');
-    plt.scatter(centers[:, 0], centers[:, 1], c='black',
-                s=200, alpha=0.5);
-    #plt.savefig('{}.png'.format(i))
 
 
 
